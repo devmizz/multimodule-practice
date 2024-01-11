3e735decff6d56fc97dc3b78ad0781c5a67ba909
devmizz <garamminchan@gmail.com>
2024-01-11 18:22:03 +0900

    create domain module

---
+++ app/domain/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    runtimeOnly("com.h2database:h2")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,10 @@ rootProject.name = "multimodule-practice"
 include(":common")
 
 include(":app")
+
 include(":app:presentation")
 
 include(":app:application")
 include(":app:application:user-application")
+
+include(":app:domain")
