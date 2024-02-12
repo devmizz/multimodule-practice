35943b63c6bc281031adbbe87895ced129262d6b
devmizz <garamminchan@gmail.com>
2024-02-13 01:09:20 +0900

    yml setting

--- app/application/user-application/src/main/kotlin/com/userapplication/service/UserService.kt
+++ app/application/user-application/src/main/kotlin/com/userapplication/service/UserService.kt
@@ -3,12 +3,19 @@ package com.userapplication.service
 import com.userdomain.entity.User
 import com.userdomain.repository.UserRepository
 import ifTrue
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
+    @Value("\${user.id}")
+    private val userId: String,
     private val userRepository: UserRepository
 ) {
+
+    init {
+        println("userId: ${userId}")
+    }
 
     fun save(randomName: String): User {
         true.ifTrue { println("this is true") }
